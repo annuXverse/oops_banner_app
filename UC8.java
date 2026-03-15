4cab8ed779789481dfd45ab08baf7487ee27d632
annuXverse <nowannu2007@gmail.com>
2026-03-15 13:50:00 +0530

    UC8: Map Collection

---
+++ UC8.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8 {
+    private static final Map<Character, String[]> patternMap = new HashMap<>();
+
+    static {
+        // Har pattern ko 6 characters ki fixed width di hai
+        patternMap.put('O', new String[]{
+            "  ***  ",
+            " *   * ",
+            " *   * ",
+            " *   * ",
+            "  ***  "
+        });
+
+        patternMap.put('P', new String[]{
+            " **** ",
+            " *  * ",
+            " **** ",
+            " *    ",
+            " *    "
+        });
+
+        patternMap.put('S', new String[]{
+            "  **** ",
+            " *     ",
+            "  ***  ",
+            "     * ",
+            " ****  "
+        });
+    }
+
+    public static void main(String[] args) {
+        renderBanner("OOPS");
+    }
+
+    public static void renderBanner(String word) {
+        word = word.toUpperCase();
+        for (int i = 0; i < 5; i++) {
+            StringBuilder line = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                if (patternMap.containsKey(c)) {
+                    // Ek extra space letters ke beech mein gap ke liye
+                    line.append(patternMap.get(c)[i]).append(" ");
+                }
+            }
+            System.out.println(line);
+        }
+    }
+}
